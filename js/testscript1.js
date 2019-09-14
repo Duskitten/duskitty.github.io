a8eb8379b47db5b950aa75a1e33c8881eda7e8ed
Duskitty <Theduskitty@gmail.com>
2019-09-14 18:55:16 -0500

    Anothertry

--- js/testscript1.js
+++ js/testscript1.js
@@ -25,35 +25,13 @@ function getCookie(cname) {
 function checkCookie() {
   var user=getCookie("theme");
   if (user != "") {
-    loadjscssfile("css/"+user+"_Theme.css","css");
+    swapStyleSheet("css/"+user+"_Theme.css")
   } else if (user == ""){
      user = "Base";
      if (user != "" && user != null) {
        console.log("loaded!")
        setCookie("theme", user, 24000000);
-       loadjscssfile("css/"+user+"_Theme.css","css");
+       swapStyleSheet("css/"+user+"_Theme.css")
      }
   }
-}
-
-
-function loadjscssfile(filename, filetype){
-    if (filetype=="js"){ //if filename is a external JavaScript file
-        var fileref=document.createElement('script')
-        fileref.setAttribute("type","text/javascript")
-        fileref.setAttribute("src", filename)
-    }
-    else if (filetype=="css"){ //if filename is an external CSS file
-      Array.prototype.forEach.call(document.querySelectorAll('style,[rel="stylesheet"],[type="text/css"]'), function(element){
-          try{
-              element.parentNode.removeChild(element)
-              }catch(err){}
-            });
-        var fileref=document.createElement("link")
-        fileref.setAttribute("rel", "stylesheet")
-        fileref.setAttribute("type", "text/css")
-        fileref.setAttribute("href", filename)
-    }
-    if (typeof fileref!="undefined")
-        document.getElementsByTagName("head")[0].appendChild(fileref)
 }
